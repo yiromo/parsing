57c9e6575830b40163a3c569696c3533acf05adb
yi <yungpxxp@gmail.com>
2024-04-25 13:52:20 +0600

    New Branch

--- src/routes/route.py
+++ src/routes/route.py
@@ -1,30 +1,14 @@
 from fastapi import APIRouter, HTTPException
-from bs4 import BeautifulSoup
-import requests
+from schemas.schema import get_url
 
 router = APIRouter(
     prefix="/pars",
     tags=["Heh"]
 )
 
-
 @router.get("/check/")
 async def parse_check(check_url: str):
     try:
-        response = requests.get(check_url)
-
-        soup = BeautifulSoup(response.content, "html.parser")
-        tables = soup.find_all('table')
-
-        parsed_data = []
-        for table in tables:
-            rows = table.find_all('tr')
-            for row in rows:
-                columns = row.find_all('td')
-                if len(columns) >= 2:
-                    check_name = columns[0].text.strip()
-                    check_price = columns[1].text.strip()
-                    parsed_data.append({"name": check_name, "info": check_price})
-        return parsed_data
+        return await get_url(check_url)
     except Exception as e:
         raise HTTPException(status_code=500)
